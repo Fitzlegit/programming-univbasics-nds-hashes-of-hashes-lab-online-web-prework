ebfc6f38a841f27461288b1c5233781071efd30a
Fitzhugh <j.rfitzhugh@gmail.com>
2019-10-21 13:34:30 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,6 +49,9 @@ def naming_system
         label: "Class",
         sub_category: { 
           label: "Order",
+          sub_category: {
+            label: "Genus",
+          }
          }
         }
       }
